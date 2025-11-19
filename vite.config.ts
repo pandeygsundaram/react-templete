ae1b75e3621052ce782bfad6d25820f071cb18d3
Sundaram <thisnewcomputerismine2020@gmail.com>
2025-11-20 00:13:22 +0530

    fix:allow req from other endpoints

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-                host: '0.0.0.0',
-                port: 5173,
-                strictPort: true,
-                hmr: {
-                  clientPort: 80
-                }
-              }
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: true,
+    hmr: {
+      clientPort: 80
+    }
+  }
 })
